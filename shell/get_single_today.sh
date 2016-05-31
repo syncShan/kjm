37a16733b8d1e8f867eee4409a784b3bea1509d5
wellesliang <liangwm@163.com>
2016-05-31 13:49:18 +0800

    dns fix

--- shell/get_single_today.sh
+++ shell/get_single_today.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 idList=$1
 date=$2
-url="http://hq.sinajs.cn/list=$idList"
+#url="http://hq.sinajs.cn/list=$idList"
+url="http://112.90.6.246/list=$idList"
 res=`curl $url >> rawdata/$date/data.csv`
 echo $res
